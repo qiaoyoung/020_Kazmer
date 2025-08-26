2ca069a275b573fbefa56ca8172c1c96571d97d9
Joe <joeyoung@guojiang.tv>
2025-08-26 17:07:45 +0800

    1.0.0+3

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,8 +7,6 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-    NSArray *arr = @[@"application", @"launchOptions", @"register"];
-    NSLog(@"current is %@", arr[3]);
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
