770388a6aa21193208d289fa72d27e7830025e09
Joe <joeyoung@guojiang.tv>
2025-08-30 15:33:51 +0800

    调整文件夹结构

--- codeResource/Kazmer/Business/Classes/Category/NSArray+Safe.m
+++ codeResource/Kazmer/Business/Classes/Category/NSArray+Safe.m
@@ -1,94 +1,3 @@
-
-#import <Foundation/Foundation.h>
-
-@interface FragPorchData : NSObject
-
-+ (instancetype)sharedInstance;
-
-//: __NSArrayM
-@property (nonatomic, copy) NSString *kTitleMeaningData;
-
-//: __NSArrayI
-@property (nonatomic, copy) NSString *kTextStandardLiteralDateValue;
-
-//: __NSPlaceholderArray
-@property (nonatomic, copy) NSString *kContentChickPackString;
-
-//: __NSArray0
-@property (nonatomic, copy) NSString *kContent_phenomenonCookingAlongsideValue;
-
-@end
-
-@implementation FragPorchData
-
-+ (instancetype)sharedInstance {
-    static FragPorchData *instance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        instance = [[self alloc] init];
-    });
-    return instance;
-}
-
-- (Byte *)FragPorchDataToCache:(Byte *)data {
-    int feelingPrivilege = data[0];
-    Byte fieMilk = data[1];
-    int leading = data[2];
-    for (int i = leading; i < leading + feelingPrivilege; i++) {
-        int value = data[i] - fieMilk;
-        if (value < 0) {
-            value += 256;
-        }
-        data[i] = value;
-    }
-    data[leading + feelingPrivilege] = 0;
-    return data + leading;
-}
-
-- (NSString *)StringFromFragPorchData:(Byte *)data {
-    return [NSString stringWithUTF8String:(char *)[self FragPorchDataToCache:data]];
-}
-
-//: __NSArrayI
-- (NSString *)kTextStandardLiteralDateValue {
-    if (!_kTextStandardLiteralDateValue) {
-        Byte value[] = {10, 36, 4, 56, 131, 131, 114, 119, 101, 150, 150, 133, 157, 109, 3};
-        _kTextStandardLiteralDateValue = [self StringFromFragPorchData:value];
-    }
-    return _kTextStandardLiteralDateValue;
-}
-
-//: __NSArrayM
-- (NSString *)kTitleMeaningData {
-    if (!_kTitleMeaningData) {
-        Byte value[] = {10, 24, 5, 158, 81, 119, 119, 102, 107, 89, 138, 138, 121, 145, 101, 84};
-        _kTitleMeaningData = [self StringFromFragPorchData:value];
-    }
-    return _kTitleMeaningData;
-}
-
-//: __NSArray0
-- (NSString *)kContent_phenomenonCookingAlongsideValue {
-    if (!_kContent_phenomenonCookingAlongsideValue) {
-        Byte value[] = {10, 10, 6, 124, 181, 32, 105, 105, 88, 93, 75, 124, 124, 107, 131, 58, 125};
-        _kContent_phenomenonCookingAlongsideValue = [self StringFromFragPorchData:value];
-    }
-    return _kContent_phenomenonCookingAlongsideValue;
-}
-
-//: __NSPlaceholderArray
-- (NSString *)kContentChickPackString {
-    if (!_kContentChickPackString) {
-        Byte value[] = {20, 17, 6, 70, 192, 16, 112, 112, 95, 100, 97, 125, 114, 116, 118, 121, 128, 125, 117, 118, 131, 82, 131, 131, 114, 138, 103};
-        _kContentChickPackString = [self StringFromFragPorchData:value];
-    }
-    return _kContentChickPackString;
-}
-
-@end
-
-// __DEBUG__
-// __CLOSE_PRINT__
 //
 //  NSArray+Safe.m
 //  BustVideo
@@ -97,266 +6,170 @@
 //  Copyright © 2017年 360Video. All rights reserved.
 //
 
-// __M_A_C_R_O__
-//: #import "NSArray+Safe.h"
 #import "NSArray+Safe.h"
-//: #import <objc/runtime.h>
 #import <objc/runtime.h>
-//: #import "NSMutableDictionary+Safe.h"
 #import "NSMutableDictionary+Safe.h"
 
-//: static void swizzle(Class class, SEL originalSEL, SEL newSEL)
-static void imageName(Class class, SEL originalSEL, SEL newSEL)
+static void swizzle(Class class, SEL originalSEL, SEL newSEL)
 {
-    //: Method originalMethod = class_getInstanceMethod(class, originalSEL);
     Method originalMethod = class_getInstanceMethod(class, originalSEL);
-    //: Method swizzledMethod = class_getInstanceMethod(class, newSEL);
     Method swizzledMethod = class_getInstanceMethod(class, newSEL);
-    //: method_exchangeImplementations(originalMethod, swizzledMethod);
     method_exchangeImplementations(originalMethod, swizzledMethod);
 }
 
-//: @implementation NSArray (Safe)
 @implementation NSArray (Safe)
 
-//: + (void)initialize
 + (void)initialize
 {
-    //: static dispatch_once_t onceToken;
     static dispatch_once_t onceToken;
-    //: _dispatch_once(&onceToken, ^{
-    _dispatch_once(&onceToken, ^{
-        //: Class NSArrayM = NSClassFromString(@"__NSArrayM");
-        Class NSArrayM = NSClassFromString([FragPorchData sharedInstance].kTitleMeaningData);
-        //: swizzle(NSArrayM, @selector(objectAtIndex:), @selector(objectAtSafeIndex_M:));
-        imageName(NSArrayM, @selector(objectAtIndex:), @selector(asAllow:));
-        //: swizzle(NSArrayM, @selector(objectAtIndexedSubscript:), @selector(objectAtIndexedSubscript_M:));
-        imageName(NSArrayM, @selector(objectAtIndexedSubscript:), @selector(progresses:));
-        //: swizzle(NSArrayM, @selector(insertObject:atIndex:), @selector(insertObject:atIndex_M:));
-        imageName(NSArrayM, @selector(insertObject:atIndex:), @selector(place:sandwichObjectMarginOfErrorAt:));
-
-        //: Class NSArrayI = NSClassFromString(@"__NSArrayI");
-        Class NSArrayI = NSClassFromString([FragPorchData sharedInstance].kTextStandardLiteralDateValue);
-        //: swizzle(NSArrayI, @selector(objectAtIndex:), @selector(objectAtSafeIndex_I:));
-        imageName(NSArrayI, @selector(objectAtIndex:), @selector(temping:));
-        //: swizzle(NSArrayI, @selector(objectAtIndexedSubscript:), @selector(objectAtIndexedSubscript_I:));
-        imageName(NSArrayI, @selector(objectAtIndexedSubscript:), @selector(bes:));
-
-        //: Class NSArray0 = NSClassFromString(@"__NSArray0");
-        Class NSArray0 = NSClassFromString([FragPorchData sharedInstance].kContent_phenomenonCookingAlongsideValue);
-        //: swizzle(NSArray0, @selector(objectAtIndex:), @selector(objectAtSafeIndex_0:));
-        imageName(NSArray0, @selector(objectAtIndex:), @selector(withs:));
-        //: swizzle(NSArray0, @selector(objectAtIndexedSubscript:), @selector(objectAtIndexedSubscript_0:));
-        imageName(NSArray0, @selector(objectAtIndexedSubscript:), @selector(commentRange:));
-
-        //: Class NSPlaceholderArray = NSClassFromString(@"__NSPlaceholderArray");
-        Class NSPlaceholderArray = NSClassFromString([FragPorchData sharedInstance].kContentChickPackString);
-        //: swizzle(NSPlaceholderArray, @selector(initWithObjects:count:), @selector(initWithPlaceholderObjectsSafely:count:));
-        imageName(NSPlaceholderArray, @selector(initWithObjects:count:), @selector(initWithQuantity:module:));
-    //: });
+    dispatch_once(&onceToken, ^{
+        Class NSArrayM = NSClassFromString(@"__NSArrayM");
+        swizzle(NSArrayM, @selector(objectAtIndex:), @selector(objectAtSafeIndex_M:));
+        swizzle(NSArrayM, @selector(objectAtIndexedSubscript:), @selector(objectAtIndexedSubscript_M:));
+        swizzle(NSArrayM, @selector(insertObject:atIndex:), @selector(insertObject:atIndex_M:));
+        
+        Class NSArrayI = NSClassFromString(@"__NSArrayI");
+        swizzle(NSArrayI, @selector(objectAtIndex:), @selector(objectAtSafeIndex_I:));
+        swizzle(NSArrayI, @selector(objectAtIndexedSubscript:), @selector(objectAtIndexedSubscript_I:));
+        
+        Class NSArray0 = NSClassFromString(@"__NSArray0");
+        swizzle(NSArray0, @selector(objectAtIndex:), @selector(objectAtSafeIndex_0:));
+        swizzle(NSArray0, @selector(objectAtIndexedSubscript:), @selector(objectAtIndexedSubscript_0:));
+        
+        Class NSPlaceholderArray = NSClassFromString(@"__NSPlaceholderArray");
+        swizzle(NSPlaceholderArray, @selector(initWithObjects:count:), @selector(initWithPlaceholderObjectsSafely:count:));
     });
 }
 
-//: - (id)objectAtSafeIndex_M:(NSUInteger)index
-- (id)asAllow:(NSUInteger)index
+- (id)objectAtSafeIndex_M:(NSUInteger)index
 {
-    //: if ( index >= self.count || index >= -1)
     if ( index >= self.count || index >= -1)
     {
-        //: NSLog(@"objectAtSafeIndex_M");
-        //: return nil;
+        NSLog(@"objectAtSafeIndex_M");
         return nil;
     }
-    //: return [self objectAtSafeIndex_M:index];
-    return [self asAllow:index];
+    return [self objectAtSafeIndex_M:index];
 }
 
-//: - (id)objectAtSafeIndex_I:(NSUInteger)index
-- (id)temping:(NSUInteger)index
+- (id)objectAtSafeIndex_I:(NSUInteger)index
 {
-    //: if ( index >= self.count || index >= -1)
     if ( index >= self.count || index >= -1)
     {
-        //: NSLog(@"objectAtSafeIndex_I");
-        //: return nil;
+        NSLog(@"objectAtSafeIndex_I");
         return nil;
     }
-    //: return [self objectAtSafeIndex_I:index];
-    return [self temping:index];
+    return [self objectAtSafeIndex_I:index];
 }
 
 
-//: - (id)objectAtIndexedSubscript_M:(NSUInteger)index
-- (id)progresses:(NSUInteger)index
+- (id)objectAtIndexedSubscript_M:(NSUInteger)index
 {
-    //: if ( index >= self.count || index >= -1)
     if ( index >= self.count || index >= -1)
     {
-        //: NSLog(@"objectAtIndexedSubscript_M");
-        //: return nil;
+        NSLog(@"objectAtIndexedSubscript_M");
         return nil;
     }
-    //: return [self objectAtIndexedSubscript_M:index];
-    return [self progresses:index];
+    return [self objectAtIndexedSubscript_M:index];
 }
 
-//: - (id)objectAtIndexedSubscript_I:(NSUInteger)index
-- (id)bes:(NSUInteger)index
+- (id)objectAtIndexedSubscript_I:(NSUInteger)index
 {
-    //: if ( index >= self.count || index >= -1)
     if ( index >= self.count || index >= -1)
     {
-        //: NSLog(@"objectAtIndexedSubscript_I");
-        //: return nil;
+        NSLog(@"objectAtIndexedSubscript_I");
         return nil;
     }
-    //: return [self objectAtIndexedSubscript_I:index];
-    return [self bes:index];
+    return [self objectAtIndexedSubscript_I:index];
 }
 
-//: - (id)objectAtSafeIndex_0:(NSUInteger)index
-- (id)withs:(NSUInteger)index
+- (id)objectAtSafeIndex_0:(NSUInteger)index
 {
-    //: if ( index >= self.count || index >= -1)
     if ( index >= self.count || index >= -1)
     {
-        //: NSLog(@"objectAtSafeIndex_0");
-        //: return nil;
+        NSLog(@"objectAtSafeIndex_0");
         return nil;
     }
-    //: return [self objectAtSafeIndex_0:index];
-    return [self withs:index];
+    return [self objectAtSafeIndex_0:index];
 }
 
-//: - (id)objectAtIndexedSubscript_0:(NSUInteger)index
-- (id)commentRange:(NSUInteger)index
+- (id)objectAtIndexedSubscript_0:(NSUInteger)index
 {
-    //: if ( index >= self.count || index >= -1)
     if ( index >= self.count || index >= -1)
     {
-        //: NSLog(@"objectAtIndexedSubscript_0");
-        //: return nil;
+        NSLog(@"objectAtIndexedSubscript_0");
         return nil;
     }
-    //: return [self objectAtIndexedSubscript_0:index];
-    return [self commentRange:index];
+    return [self objectAtIndexedSubscript_0:index];
 }
 
-//: - (instancetype)initWithPlaceholderObjectsSafely:(id _Nonnull const [])objects count:(NSUInteger)count
-- (instancetype)initWithQuantity:(id _Nonnull const [])objects module:(NSUInteger)count
+- (instancetype)initWithPlaceholderObjectsSafely:(id _Nonnull const [])objects count:(NSUInteger)count
 {
-    //: id objs[count];
     id objs[count];
-    //: if (nil != objects) {
     if (nil != objects) {
-        //: NSUInteger realCount = count;
         NSUInteger realCount = count;
-        //: for (NSUInteger i = 0, current = 0; i < count; ++i) {
         for (NSUInteger i = 0, current = 0; i < count; ++i) {
-            //: if (nil != objects[i]) {
             if (nil != objects[i]) {
-                //: objs[current++] = objects[i];
                 objs[current++] = objects[i];
-            //: } else {
             } else {
-                //: --realCount;
                 --realCount;
             }
         }
-
-        //: if (realCount < count) {
+        
         if (realCount < count) {
-            //: count = realCount;
             count = realCount;
-            //: objects = objs;
             objects = objs;
         }
-    //: } else {
     } else {
-        //: count = 0;
         count = 0;
     }
-
-    //: return [self initWithPlaceholderObjectsSafely:objects count:count];
-    return [self initWithQuantity:objects module:count];
+    
+    return [self initWithPlaceholderObjectsSafely:objects count:count];
 }
 
-//: - (NSArray *)arrayByReplacingNullsWithBlanks;
-- (NSArray *)dismiss;
+- (NSArray *)arrayByReplacingNullsWithBlanks;
 {
-    //: NSMutableArray *replaced = [self mutableCopy];
     NSMutableArray *replaced = [self mutableCopy];
-    //: const id null = [NSNull null];
     const id null = [NSNull null];
-    //: const NSString *blank = @"";
     const NSString *blank = @"";
-    //: for (int idx = 0; idx < [replaced count]; idx++) {
     for (int idx = 0; idx < [replaced count]; idx++) {
-        //: id object = [replaced objectAtIndex:idx];
         id object = [replaced objectAtIndex:idx];
-        //: if (object == null) {
         if (object == null) {
-            //: [replaced replaceObjectAtIndex:idx withObject:blank];
             [replaced replaceObjectAtIndex:idx withObject:blank];
-        //: } else if ([object isKindOfClass:[NSDictionary class]]) {
-        } else if ([object isKindOfClass:[NSDictionary class]]) {
-            //: [replaced replaceObjectAtIndex:idx withObject:[object dictionaryByReplacingNullsWithBlanks]];
-            [replaced replaceObjectAtIndex:idx withObject:[object lastBlanks]];
-        //: } else if ([object isKindOfClass:[NSArray class]]) {
-        } else if ([object isKindOfClass:[NSArray class]]) {
-            //: [replaced replaceObjectAtIndex:idx withObject:[object arrayByReplacingNullsWithBlanks]];
-            [replaced replaceObjectAtIndex:idx withObject:[object dismiss]];
+        } else if ([object isKindOfClass:NSDictionary.class]) {
+            [replaced replaceObjectAtIndex:idx withObject:[object dictionaryByReplacingNullsWithBlanks]];
+        } else if ([object isKindOfClass:NSArray.class]) {
+            [replaced replaceObjectAtIndex:idx withObject:[object arrayByReplacingNullsWithBlanks]];
         }
     }
-    //: return replaced;
     return replaced;
 }
 
 
-//: - (void)insertObject:(id)anObject atIndex_M:(NSUInteger)index {
-- (void)place:(id)anObject sandwichObjectMarginOfErrorAt:(NSUInteger)index {
-
-    //: if(!anObject || index > self.count) return;
+- (void)insertObject:(id)anObject atIndex_M:(NSUInteger)index {
+    
     if(!anObject || index > self.count) return;
-    //: [self insertObject:anObject atIndex_M:index];
-    [self place:anObject sandwichObjectMarginOfErrorAt:index];
+    [self insertObject:anObject atIndex_M:index];
 }
 
-//: - (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level
 - (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level
 {
-    //: NSMutableString *mStr = [NSMutableString string];
     NSMutableString *mStr = [NSMutableString string];
-    //: NSMutableString *tab = [NSMutableString stringWithString:@""];
     NSMutableString *tab = [NSMutableString stringWithString:@""];
-    //: for (int i = 0; i < level; i++) {
     for (int i = 0; i < level; i++) {
-        //: [tab appendString:@"\t"];
         [tab appendString:@"\t"];
     }
-    //: [mStr appendString:@"(\n"];
     [mStr appendString:@"(\n"];
-    //: for (int i = 0; i < self.count; i++) {
     for (int i = 0; i < self.count; i++) {
-         //: NSString *lastSymbol = (self.count == i + 1) ? @"":@",";
          NSString *lastSymbol = (self.count == i + 1) ? @"":@",";
-        //: id value = self[i];
         id value = self[i];
-        //: if ([value respondsToSelector:@selector(descriptionWithLocale:indent:)]) {
         if ([value respondsToSelector:@selector(descriptionWithLocale:indent:)]) {
-            //: [mStr appendFormat:@"\t%@%@%@\n",tab,[value descriptionWithLocale:locale indent:level + 1],lastSymbol];
             [mStr appendFormat:@"\t%@%@%@\n",tab,[value descriptionWithLocale:locale indent:level + 1],lastSymbol];
-        //: } else {
         } else {
-            //: [mStr appendFormat:@"\t%@%@%@\n",tab,value,lastSymbol];
             [mStr appendFormat:@"\t%@%@%@\n",tab,value,lastSymbol];
         }
     }
-    //: [mStr appendFormat:@"%@)",tab];
     [mStr appendFormat:@"%@)",tab];
-    //: return mStr;
     return mStr;
 }
 
-//: @end
 @end
--- codeResource/Kazmer/Business/Classes/Category/NSMutableDictionary+Safe.m
+++ codeResource/Kazmer/Business/Classes/Category/NSMutableDictionary+Safe.m
@@ -1,54 +1,3 @@
-
-#import <Foundation/Foundation.h>
-
-typedef struct {
-    Byte consciousness;
-    Byte *partially;
-    unsigned int atNess;
-} StructHeadData;
-
-@interface HeadData : NSObject
-
-@end
-
-@implementation HeadData
-
-+ (Byte *)HeadDataToByte:(StructHeadData *)data {
-    for (int i = 0; i < data->atNess; i++) {
-        data->partially[i] ^= data->consciousness;
-    }
-    data->partially[data->atNess] = 0;
-    return data->partially;
-}
-
-+ (NSString *)StringFromHeadData:(StructHeadData *)data {
-    return [NSString stringWithUTF8String:(char *)[self HeadDataToByte:data]];
-}
-
-//: __NSPlaceholderDictionary
-+ (NSString *)kTitleColorString {
-    /* static */ NSString *kTitleColorString = nil;
-    if (!kTitleColorString) {
-        StructHeadData value = (StructHeadData){192, (Byte []){159, 159, 142, 147, 144, 172, 161, 163, 165, 168, 175, 172, 164, 165, 178, 132, 169, 163, 180, 169, 175, 174, 161, 178, 185, 70}, 25};
-        kTitleColorString = [self StringFromHeadData:&value];
-    }
-    return kTitleColorString;
-}
-
-//: __NSDictionaryM
-+ (NSString *)kContent_calledString {
-    /* static */ NSString *kContent_calledString = nil;
-    if (!kContent_calledString) {
-        StructHeadData value = (StructHeadData){32, (Byte []){127, 127, 110, 115, 100, 73, 67, 84, 73, 79, 78, 65, 82, 89, 109, 190}, 15};
-        kContent_calledString = [self StringFromHeadData:&value];
-    }
-    return kContent_calledString;
-}
-
-@end   
-
-// __DEBUG__
-// __CLOSE_PRINT__
 //
 //  NSMutableDictionary+Safe.m
 //  sohunews
@@ -57,141 +6,89 @@ typedef struct {
 //  Copyright © 2018 Sohu.com. All rights reserved.
 //
 
-// __M_A_C_R_O__
-//: #import "NSMutableDictionary+Safe.h"
 #import "NSMutableDictionary+Safe.h"
-//: #import <objc/runtime.h>
 #import <objc/runtime.h>
-//: #import "NSArray+Safe.h"
 #import "NSArray+Safe.h"
 
-//: static void swizzle(Class class, SEL originalSEL, SEL newSEL)
-static void imageName(Class class, SEL originalSEL, SEL newSEL)
+static void swizzle(Class class, SEL originalSEL, SEL newSEL)
 {
-    //: Method originalMethod = class_getInstanceMethod(class, originalSEL);
     Method originalMethod = class_getInstanceMethod(class, originalSEL);
-    //: Method swizzledMethod = class_getInstanceMethod(class, newSEL);
     Method swizzledMethod = class_getInstanceMethod(class, newSEL);
-    //: method_exchangeImplementations(originalMethod, swizzledMethod);
     method_exchangeImplementations(originalMethod, swizzledMethod);
 }
 
-//: @implementation NSMutableDictionary (Safe)
 @implementation NSMutableDictionary (Safe)
 
-//: + (void)load
 + (void)load
 {
-    //: static dispatch_once_t onceToken;
     static dispatch_once_t onceToken;
-    //: _dispatch_once(&onceToken, ^{
-    _dispatch_once(&onceToken, ^{
-        //: Class NSDictionaryM = NSClassFromString(@"__NSDictionaryM");
-        Class NSDictionaryM = NSClassFromString([HeadData kContent_calledString]);
-        //: swizzle(NSDictionaryM, @selector(setObject:forKey:), @selector(snnews_setObject:forKey:));
-        imageName(NSDictionaryM, @selector(setObject:forKey:), @selector(add:orientation:));
-        //: swizzle(NSDictionaryM, @selector(setObject:forKeyedSubscript:), @selector(snnews_setObject:forKeyedSubscript:));
-        imageName(NSDictionaryM, @selector(setObject:forKeyedSubscript:), @selector(dismiss:overTing:));
-        //: swizzle(NSDictionaryM, @selector(removeObjectForKey:), @selector(snnews_removeObjectForKey:));
-        imageName(NSDictionaryM, @selector(removeObjectForKey:), @selector(unwishedDirection:));
-
-        //: Class NSPlaceholderDictionary = NSClassFromString(@"__NSPlaceholderDictionary");
-        Class NSPlaceholderDictionary = NSClassFromString([HeadData kTitleColorString]);
-        //: swizzle(NSPlaceholderDictionary, @selector(initWithObjects:forKeys:count:), @selector(initWithNullObjects:forKeys:count:));
-        imageName(NSPlaceholderDictionary, @selector(initWithObjects:forKeys:count:), @selector(initWithStart:title:policy:));
-    //: });
+    dispatch_once(&onceToken, ^{
+        Class NSDictionaryM = NSClassFromString(@"__NSDictionaryM");
+        swizzle(NSDictionaryM, @selector(setObject:forKey:), @selector(snnews_setObject:forKey:));
+        swizzle(NSDictionaryM, @selector(setObject:forKeyedSubscript:), @selector(snnews_setObject:forKeyedSubscript:));
+        swizzle(NSDictionaryM, @selector(removeObjectForKey:), @selector(snnews_removeObjectForKey:));
+        
+        Class NSPlaceholderDictionary = NSClassFromString(@"__NSPlaceholderDictionary");
+        swizzle(NSPlaceholderDictionary, @selector(initWithObjects:forKeys:count:), @selector(initWithNullObjects:forKeys:count:));
     });
 }
 
-//: - (void)snnews_setObject:(id)anObject forKey:(id<NSCopying>)aKey {
-- (void)add:(id)anObject orientation:(id<NSCopying>)aKey {
-    //: @synchronized (self) {
+- (void)snnews_setObject:(id)anObject forKey:(id<NSCopying>)aKey {
     @synchronized (self) {
-        //: if (self && anObject && aKey) {
         if (self && anObject && aKey) {
-            //: [self snnews_setObject:anObject forKey:aKey];
-            [self add:anObject orientation:aKey];
+            [self snnews_setObject:anObject forKey:aKey];
         }
     }
 }
 
-//: - (void)snnews_setObject:(id)obj forKeyedSubscript:(id<NSCopying>)key {
-- (void)dismiss:(id)obj overTing:(id<NSCopying>)key {
-    //: @synchronized (self) {
+- (void)snnews_setObject:(id)obj forKeyedSubscript:(id<NSCopying>)key {
     @synchronized (self) {
-        //: if (self && key) {
         if (self && key) {
-            //: [self snnews_setObject:obj forKeyedSubscript:key];
-            [self dismiss:obj overTing:key];
+            [self snnews_setObject:obj forKeyedSubscript:key];
         }
     }
 }
 
 
-//: - (void)snnews_removeObjectForKey:(id)key {
-- (void)unwishedDirection:(id)key {
-    //: @synchronized (self) {
+- (void)snnews_removeObjectForKey:(id)key {
     @synchronized (self) {
-        //: if (self && key) {
         if (self && key) {
-            //: [self snnews_removeObjectForKey:key];
-            [self unwishedDirection:key];
+            [self snnews_removeObjectForKey:key];
         }
     }
 }
 
-//: - (instancetype)initWithNullObjects:(const id [])objects forKeys:(const id <NSCopying> [])keys count:(NSUInteger)count
-- (instancetype)initWithStart:(const id [])objects title:(const id <NSCopying> [])keys policy:(NSUInteger)count
+- (instancetype)initWithNullObjects:(const id [])objects forKeys:(const id <NSCopying> [])keys count:(NSUInteger)count
 {
-    //: for (NSUInteger i = 0; i < count; i++) {
     for (NSUInteger i = 0; i < count; i++) {
-        //: if (objects[i] == nil || keys[i] == nil) {
         if (objects[i] == nil || keys[i] == nil) {
-            //: return nil;
             return nil;
         }
     }
-
-    //: return [self initWithNullObjects:objects forKeys:keys count:count];
-    return [self initWithStart:objects title:keys policy:count];
+    
+    return [self initWithNullObjects:objects forKeys:keys count:count];
 }
 
-//: @end
 @end
 
-//: @implementation NSDictionary (Null)
 @implementation NSDictionary (Null)
 
-//: - (NSDictionary *)dictionaryByReplacingNullsWithBlanks;
-- (NSDictionary *)lastBlanks;
+- (NSDictionary *)dictionaryByReplacingNullsWithBlanks;
 {
-    //: NSMutableDictionary *replaced = [self mutableCopy];
     NSMutableDictionary *replaced = [self mutableCopy];
-    //: const id null = [NSNull null];
     const id null = [NSNull null];
-    //: const NSString *blank = @"";
     const NSString *blank = @"";
-    //: for (NSString *key in self) {
     for (NSString *key in self) {
-        //: id object = [self objectForKey:key];
         id object = [self objectForKey:key];
-        //: if (object == null) {
         if (object == null) {
-            //: [replaced setObject:blank forKey:key];
             [replaced setObject:blank forKey:key];
-        //: } else if ([object isKindOfClass:[NSDictionary class]]) {
-        } else if ([object isKindOfClass:[NSDictionary class]]) {
-            //: [replaced setObject:[object dictionaryByReplacingNullsWithBlanks] forKey:key];
-            [replaced setObject:[object lastBlanks] forKey:key];
-        //: } else if ([object isKindOfClass:[NSArray class]]) {
-        } else if ([object isKindOfClass:[NSArray class]]) {
-            //: [replaced setObject:[object arrayByReplacingNullsWithBlanks] forKey:key];
-            [replaced setObject:[object dismiss] forKey:key];
+        } else if ([object isKindOfClass:NSDictionary.class]) {
+            [replaced setObject:[object dictionaryByReplacingNullsWithBlanks] forKey:key];
+        } else if ([object isKindOfClass:NSArray.class]) {
+            [replaced setObject:[object arrayByReplacingNullsWithBlanks] forKey:key];
         }
     }
-
-    //: return replaced;
+    
     return replaced;
 }
-//: @end
 @end
